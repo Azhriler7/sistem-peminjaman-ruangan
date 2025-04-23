5204593050c9b20052cf0c496b350a8be8154082
Azzahra Fitri Ramadhanti <azzahrafitriramadhanti@gmail.com>
2025-04-23 23:00:33 +0700

    Push UI frontend

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->file(public_path('index.html'));
 });
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
